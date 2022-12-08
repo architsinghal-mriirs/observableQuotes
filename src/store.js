9528bfb1f3c49ffeaf5ff745fb22f407635dc633
archit-mriirs <architsinghal_mriirs@outlook.com>
2022-12-08 15:44:47 +0530

    changes

--- src/store.js
+++ src/store.js
@@ -1,3 +1,10 @@
-import {BehaviourSubject} from "rxjs"
 
-const rawQuotes$ = new BehaviourSubject({"id": 1, "quote": "Quote1"})
+import {Observable, of } from 'rxjs';
+
+
+// const streamA$ = Observable.of(2);
+// const streamB$ = Observable.of(4);
+// const streamC$ = Observable.concat(streamA$, streamB$)
+//   .reduce((x, y) => x  + y);
+
+// streamC$.subscribe(console.log);
